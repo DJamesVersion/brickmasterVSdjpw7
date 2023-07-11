10d1d8b2080edd0f0fdd81bd6951da059995c81c
tahkmahnelle <137662409+DJamesVersion@users.noreply.github.com>
2023-07-11 08:14:26 -0700

    Add files via upload

---
+++ Microwavable-Phaeries.sh
@@ -0,0 +1,13 @@
+# This is a shell archive.  Save it in a file, remove anything before
+# this line, and then unpack it by entering "sh file".  Note, it may
+# create directories; files and directories will be owned by you and
+# have default permissions.
+#
+# This archive contains:
+#
+#	./
+#
+echo c - ./
+mkdir -p ./ > /dev/null 2>&1
+exit
+
